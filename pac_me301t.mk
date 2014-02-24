8fd21bc1b0a6991cc9a573a3b80982a369359ed1
jesse131 <jesse@harjunpaa.fi>
2014-02-24 16:46:41 +0200

    Create PAC_ME301T
    Move this file to vendor/pac/products/.
    I created this for other people to use.

---
+++ pac_me301t.mk
@@ -0,0 +1,28 @@
+# Check for target product
+ifeq (pac_me301t,$(TARGET_PRODUCT))
+
+# OVERLAY_TARGET adds overlay asset source
+OVERLAY_TARGET := pa_me301t
+
+# AOKP device overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/pac/overlay/aokp/common_tablet
+
+# PAC device overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/pac/overlay/pac/mdpi
+
+# PA device overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/pa/overlay/pa_me301t
+
+# Copy bootanimation
+PRODUCT_COPY_FILES += \
+    vendor/pac/prebuilt/1280x800/bootanimation.zip:system/media/bootanimation.zip
+
+# include PAC common configuration
+include vendor/pac/config/pac_common.mk
+
+# Inherit CM device configuration
+$(call inherit-product, device/asus/me301t/cm.mk)
+
+PRODUCT_NAME := pac_me301t
+
+endif
